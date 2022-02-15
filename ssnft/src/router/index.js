730b4142387ad0772008d33eba9325a0e516597d
tryDifferent <zhfei8888@163.com>
2022-02-16 00:13:57 +0800

    [add] 添加个人信息页面

--- ssnft/src/router/index.js
+++ ssnft/src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/Home.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   }
 ]
 
