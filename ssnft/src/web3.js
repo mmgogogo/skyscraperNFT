fb901b20e9ad25323b64fe26cb42a0d15aa7235d
m <m@gmail.com>
2022-02-16 09:44:15 +0800

    test:recovery code

--- ssnft/src/web3.js
+++ ssnft/src/web3.js
@@ -14,7 +14,7 @@ _.assign(1, 1)
 /**
  * 合约地址
  */
-// const contractAddress = '0x93893eB7a1eBB90ED99b0FcEE48b5171aADc2b06' // Kovan
+const contractAddress = '0x93893eB7a1eBB90ED99b0FcEE48b5171aADc2b06' // Kovan
 // const contractUrl = 'https://kovan.etherscan.io/address/0x93893eb7a1ebb90ed99b0fcee48b5171aadc2b06'
 
 /**
@@ -30,8 +30,8 @@ _.assign(1, 1)
 // const networkHttpProvider = 'https://kovan.infura.io/v3/' + providerToken
 
 // alchemy
-const contractAddress = '0xFD0B9c88DF4A884Eee463B7DBb46d97c53fa757B'
-const web3HttpProvider = 'https://eth-ropsten.alchemyapi.io/v2/Po-F6eE3SaJQ9R74LUWLa1gOW36CTh7J'
+// const contractAddress = '0xFD0B9c88DF4A884Eee463B7DBb46d97c53fa757B'
+// const web3HttpProvider = 'https://eth-ropsten.alchemyapi.io/v2/Po-F6eE3SaJQ9R74LUWLa1gOW36CTh7J'
 
 /**
  * ABI
@@ -53,15 +53,18 @@ const Dapp = {
     try {
       if (isMetaMaskInstalled()) {
         if (window.ethereum.chainId !== targetChainId) {
-          // alert('链ID ' + targetChainId + ' 不是 主网！请在钱包中切换')
-          // return Dapp
+          alert('链ID ' + targetChainId + ' 不是 主网！请在钱包中切换')
+          return Dapp
         }
         Dapp.Bridges.ethereum = window.ethereum
-        // Dapp.Bridges.local = new ethers.providers.Web3Provider(window.ethereum)
-        Dapp.Bridges.local = new ethers.providers.Web3Provider(web3HttpProvider)
+        Dapp.Bridges.local = new ethers.providers.Web3Provider(window.ethereum)
+        // Dapp.Bridges.local = new ethers.providers.Web3Provider(web3HttpProvider)
+        console.log('Dapp - 11 ==>', Dapp)
         Dapp.Bridges.reader = new ethers.Contract(contractAddress, contractAbi, Dapp.Bridges.local)
+        console.log('Dapp - 22 ==>', Dapp)
         Dapp.Bridges.signer = Dapp.Bridges.local.getSigner() // 钱包签名
         Dapp.Bridges.writer = Dapp.Bridges.reader.connect(Dapp.Bridges.signer)
+        console.log('Dapp - 33 ==>', Dapp)
 
         return Dapp
       }
