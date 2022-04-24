0f17b84e19052724d911ab3a664149b0261623f3
tryDifferent <zhfei8888@163.com>
2022-04-24 12:06:19 +0800

    [update] 1. add deploy.bat

---
+++ ssnft/deploy.bat
@@ -0,0 +1,31 @@
+# deploy windows
+
+## build app
+yarn run build
+
+cp -R CNAME dist/
+cp -R doc.md dist/
+
+## navigate into the build output directory
+cd dist
+
+# #if you are deploying to a custom domain
+## echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:pzhello/pzhello.github.io.git master:gh-pages
+
+cd ../
+
+# echo "# pzhello.github.io" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/pzhello/pzhello.github.io.git
+# git push -u origin main
+
+# git remote add origin https://github.com/pzhello/pzhello.github.io.git
+# git branch -M main
+# git push -u origin main
