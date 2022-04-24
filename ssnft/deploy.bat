5d94a29dac99314cba8d8698daef056182337923
tryDifferent <zhfei8888@163.com>
2022-04-24 13:29:40 +0800

    [uddate] update .bat file comment information

--- ssnft/deploy.bat
+++ ssnft/deploy.bat
@@ -1,31 +1,18 @@
-# deploy windows
+:: deploy windows
 
-## build app
+:: build app
 yarn run build
 
 cp -R CNAME dist/
 cp -R doc.md dist/
 
-## navigate into the build output directory
+:: navigate into the build output directory
 cd dist
 
-# #if you are deploying to a custom domain
-## echo 'www.example.com' > CNAME
+:: #if you are deploying to a custom domain
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:pzhello/pzhello.github.io.git master:gh-pages
 
 cd ../
-
-# echo "# pzhello.github.io" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/pzhello/pzhello.github.io.git
-# git push -u origin main
-
-# git remote add origin https://github.com/pzhello/pzhello.github.io.git
-# git branch -M main
-# git push -u origin main
