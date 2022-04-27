9cd8a2078489ae9edf012232fb5861c375542cdb
ElevenZhou <zhhq15332@sina.com>
2022-04-27 16:33:32 +0800

    Block ctrl+a
    Block ctrl+a

--- ssnft/src/App.vue
+++ ssnft/src/App.vue
@@ -10,5 +10,22 @@ export default {
   }
 }
 </script>
+
+<script>
+   document.onkeydown = function(event){
+        if (event.ctrlKey && window.event.keyCode==65){   //禁用ctrl + a 功能
+            return false;
+        }        
+             
+        if (event.ctrlKey && window.event.keyCode==83){	//禁用ctrl + s 功能
+            return false;
+        }
+
+        if (event.ctrlKey && window.event.keyCode==86){	//禁用ctrl + v 功能
+           return false;
+        }
+    }
+</script>
+
 <style>
 </style>
